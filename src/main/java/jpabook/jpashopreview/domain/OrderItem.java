14fcd7c1fb08f694884fc095147ee80272ce88ca
yangbyoungseok <ybs991@naver.com>
2022-07-06 21:01:08 +0900

    도메인 분석 설계

---
+++ src/main/java/jpabook/jpashopreview/domain/OrderItem.java
@@ -0,0 +1,30 @@
+package jpabook.jpashopreview.domain;
+
+import jpabook.jpashopreview.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_item_id")
+    private Long id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice;
+    private int count;
+
+}
--- src/test/java/jpabook/jpashopreview/repository/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashopreview/repository/MemberRepositoryTest.java
@@ -1,13 +1,10 @@
 package jpabook.jpashopreview.repository;
 
-import jpabook.jpashopreview.entity.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MemberRepositoryTest {
